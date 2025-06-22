66d5870b138d02bf7f3cdc1e1e5be7d6eccbef1b
Dionatã Santos <dionatasantos@hotmail.com>
2025-06-22 19:08:03 -0300

    onlinePaymentService terminando

--- src/services/ContractService.java
+++ src/services/ContractService.java
@@ -1,8 +1,12 @@
 package services;
 
+import java.time.LocalDate;
+
 import entities.Contract;
+import entities.Installment;
 
 public class ContractService {
+	
 	private OnlinePaymentService onlinePaymentService;
 
 	public ContractService(OnlinePaymentService onlinePaymentService) {
@@ -11,6 +15,20 @@ public class ContractService {
 	
 	public void processContract(Contract contract, int months) {
 		
+		double basicQuota = contract.getTotalValue() / months;
+		
+		for (int i=0; i < months; i ++) {
+			LocalDate dueDate = contract.getDate().plusMonths(i);
+			
+			double interest = onlinePaymentService.interest(basicQuota, i);
+			
+			double fee = onlinePaymentService.paymentFee(basicQuota + interest);
+			
+			double quota = basicQuota + interest + fee;
+			
+			contract.getInstallments().add(new Installment(dueDate, quota)); 
+			
+		}
 	}
 	
 	
